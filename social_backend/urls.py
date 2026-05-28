cbae0f6f2feb5db4b003d80263fef5c7a73c7d8b
mantisg <joshuabliven93@gmail.com>
2026-05-28 19:37:01 -0400

    Add missing import to social_backend/urls.py

--- social_backend/urls.py
+++ social_backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, ToggleReactionView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
