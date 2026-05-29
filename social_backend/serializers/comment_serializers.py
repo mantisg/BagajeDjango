66da4a900d1893a3b552b0ccdd31a47be3a0768f
mantisg <joshuabliven93@gmail.com>
2026-05-29 19:39:02 -0400

    Create a more fleshed out and separate comment serializer

--- social_backend/serializers/comment_serializers.py
+++ social_backend/serializers/comment_serializers.py
@@ -2,9 +2,32 @@ from rest_framework import serializers
 from ..models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+
+    author_username = serializers.CharField(
+        source='author.username',
+        read_only=True
+    )
+
+    author_profile_picture = serializers.ImageField(
+        source='author.profile_picture',
+        read_only=True
+    )
 
     class Meta:
         model = Comment
 
-        fields = '__all__'
+        fields = [
+            'id',
+            'post',
+            'author',
+            'author_username',
+            'author_profile_picture',
+            'content',
+            'created_at',
+            'is_deleted',
+        ]
+
+        read_only_fields = [
+            'author',
+            'created_at',
+        ]
