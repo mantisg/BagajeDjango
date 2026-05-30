27958a8e87da3f7463ef491741772da031623a2b
mantisg <joshuabliven93@gmail.com>
2026-05-30 18:13:25 -0400

    Add permission logic for comments

--- social_backend/permissions.py
+++ social_backend/permissions.py
@@ -16,3 +16,19 @@ class IsCreatorOrReadOnly(BasePermission):
             request.user.is_authenticated
             and request.user.is_creator
         )
+    
+class IsCommentOwnerOrCreator(BasePermission):
+    """
+    Comment owner can edit/delete.
+    Site creator can edit/delete any comment.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        return (
+            obj.author == request.user
+            or request.user.is_creator
+        )
