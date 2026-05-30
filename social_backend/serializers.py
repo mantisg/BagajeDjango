92954c1c455daf428b471ed0183164e0c1554458
mantisg <joshuabliven93@gmail.com>
2026-05-29 20:09:34 -0400

    Remove old serializers.py

--- social_backend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'author', 'title', 'content', 'image', 'video', 'created_at', 'updated_at']
