c7cce3860779c6de166adb08eb905064f08251a8
mantisg <joshuabliven93@gmail.com>
2026-05-30 13:49:25 -0400

    Register models in app registry

--- social_backend/admin.py
+++ social_backend/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, User, Comment, Reaction, UserActivity
 
 admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Reaction)
+admin.site.register(UserActivity)
