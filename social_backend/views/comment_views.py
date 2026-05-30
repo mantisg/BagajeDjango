8829d3bcc3f1ae86dd1bd696b407ca5741709283
mantisg <joshuabliven93@gmail.com>
2026-05-30 18:14:53 -0400

    Create view for comments

---
+++ social_backend/views/comment_views.py
@@ -0,0 +1,31 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from ..models import Post, Comment
+from ..serializers.comment_serializers import CommentSerializer
+
+
+class PostCommentListCreateView(
+    generics.ListCreateAPIView
+):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+
+        slug = self.kwargs['slug']
+
+        return Comment.objects.filter(
+            post__slug=slug,
+            is_deleted=False
+        ).order_by('created_at')
+
+    def perform_create(self, serializer):
+
+        slug = self.kwargs['slug']
+
+        post = Post.objects.get(slug=slug)
+
+        serializer.save(
+            author=self.request.user,
+            post=post
+        )
