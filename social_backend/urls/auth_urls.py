dfc2e3dd263ad56ea669f87947170e03f826d65e
mantisg <joshuabliven93@gmail.com>
2026-05-30 02:49:57 -0400

    Add auth urls for registration, login, logout, and current user

---
+++ social_backend/urls/auth_urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+
+from ..views.auth_views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    CurrentUserView
+)
+
+urlpatterns = [
+    path(
+        'register/',
+        RegisterView.as_view(),
+        name='register'
+    ),
+
+    path(
+        'login/',
+        LoginView.as_view(),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout'
+    ),
+
+    path(
+        'me/',
+        CurrentUserView.as_view(),
+        name='current-user'
+    ),
+]
