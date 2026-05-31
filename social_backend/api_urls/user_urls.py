9ebea960ea2ad30e9183ae4ed0c09a78e1e517fa
mantisg <joshuabliven93@gmail.com>
2026-05-31 17:30:51 -0400

    Create user urls

--- social_backend/api_urls/user_urls.py
+++ social_backend/api_urls/user_urls.py
@@ -1,3 +1,20 @@
 from django.urls import path
+from ..views.user_views import (
+    CurrentUserView,
+    UserProfileView,
+)
 
-urlpatterns = []
+urlpatterns = [
+
+    path(
+        'me/',
+        CurrentUserView.as_view(),
+        name='current-user'
+    ),
+
+    path(
+        '<slug:slug>/',
+        UserProfileView.as_view(),
+        name='user-profile'
+    ),
+]
