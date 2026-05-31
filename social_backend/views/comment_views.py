5f9ac06c73dd56487fa5db466aa09cdf49c754fa
mantisg <joshuabliven93@gmail.com>
2026-05-30 21:56:22 -0400

    Add delete and update functions to comment view

--- social_backend/views/comment_views.py
+++ social_backend/views/comment_views.py
@@ -2,11 +2,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from ..models import Post, Comment
 from ..serializers.comment_serializers import CommentSerializer
+from ..permissions import IsCommentOwnerOrCreator
 
 
-class PostCommentListCreateView(
-    generics.ListCreateAPIView
-):
+class PostCommentListCreateView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
@@ -29,3 +28,8 @@ class PostCommentListCreateView(
             author=self.request.user,
             post=post
         )
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsCommentOwnerOrCreator]
