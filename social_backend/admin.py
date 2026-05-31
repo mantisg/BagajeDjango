ffb223fa6902e8d06b49a9f28e8e0d8c7eb11b06
mantisg <joshuabliven93@gmail.com>
2026-05-31 07:43:49 -0400

    Register SavedPost in admin and clean up files

--- social_backend/admin.py
+++ social_backend/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, User, Comment, Reaction, UserActivity
+from .models import Post, User, Comment, Reaction, UserActivity, SavedPost
 
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(Comment)
 admin.site.register(Reaction)
 admin.site.register(UserActivity)
+admin.site.register(SavedPost)
--- social_backend/views/saved_post_views.py
+++ social_backend/views/saved_post_views.py
@@ -1,10 +1,11 @@
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from ..serializers.saved_post_serializers import SavedPostSerializer
 from ..models import Post, SavedPost
 
 class ToggleSavedPostView(APIView):
-
     permission_classes = [IsAuthenticated]
 
     def post(self, request, post_id):
@@ -31,3 +32,17 @@ class ToggleSavedPostView(APIView):
         return Response({
             'status': 'saved'
         })
+    
+class SavedPostListView(generics.ListAPIView):
+    serializer_class = SavedPostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+
+        return SavedPost.objects.filter(
+            user=self.request.user
+        ).select_related(
+            'post'
+        ).order_by(
+            '-created_at'
+        )
