d41c80ef39c5a108170b4820fec21e7a2eaee1c7
mantisg <joshuabliven93@gmail.com>
2026-05-31 17:33:24 -0400

    Add public version of user serializer

--- social_backend/serializers/user_serializers.py
+++ social_backend/serializers/user_serializers.py
@@ -22,3 +22,22 @@ class UserSerializer(serializers.ModelSerializer):
             'is_creator',
             'created_at',
         ]
+
+class PublicUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+
+        fields = [
+            'id',
+            'username',
+            'slug',
+            'profile_picture',
+            'is_creator',
+        ]
+
+        read_only_fields = [
+            'id',
+            'slug',
+            'is_creator',
+        ]
