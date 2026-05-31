e8901e74a3a4545a617694560d4613b88e351fb3
mantisg <joshuabliven93@gmail.com>
2026-05-31 17:35:02 -0400

    Update profile view to use public serializer

--- social_backend/views/user_views.py
+++ social_backend/views/user_views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from ..models import User
-from ..serializers.user_serializers import UserSerializer
+from ..serializers.user_serializers import UserSerializer, PublicUserSerializer
 
 class CurrentUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
@@ -14,7 +14,7 @@ class UserProfileView(
     generics.RetrieveAPIView
 ):
 
-    serializer_class = UserSerializer
+    serializer_class = PublicUserSerializer
     lookup_field = 'slug'
 
     queryset = User.objects.all()
